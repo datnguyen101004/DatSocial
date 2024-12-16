3ddcd0b2bead00e12d1c2d79e78455bb2f030504
Dat Pham <datnguyen101004@gmail.com>
2024-12-17 00:57:49 +0700

    jwtToken header

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -2,12 +2,24 @@ import "./App.css";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
+  const isAuthenticated = !!localStorage.getItem("jwtToken"); // Kiểm tra token trong localStorage
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* Chỉ cho phép truy cập /home nếu đã đăng nhập */}
+        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+        {/* Nếu người dùng truy cập root, điều hướng đến /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
--- Frontend/frontend/src/components/Navbar.jsx
+++ Frontend/frontend/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./Css/Navbar.css"; // Import CSS
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <Link to="/" className="navbar-brand">DatSocial</Link>
+      <ul className="navbar-menu">
+        <li><Link to="/login">login</Link></li>
+        <li><Link to="/register">signup</Link></li> 
+        <li>
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
